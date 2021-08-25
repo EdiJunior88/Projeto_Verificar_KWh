ba81db04a012c53648575b28538b76c820b9ab8f
EdiJunior88 <er.moura88@yahoo.com.br>
2021-08-25 20:52:15 -0300

    JAVASCRIPT

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,28 +1,50 @@
 
 function confirmar() {
-  var tarifaTotal = document.getElementById('tarifa')
-  var consumoTotal = document.getElementById('consumo')
+  var nomeEletro = document.getElementById('eletro')
+  var potenciaTotal = document.getElementById('potencia')
+  var consumoDia = document.getElementById('horasDia')
+  var consumoMes = document.getElementById('diasMes')
   var resultadoTotal = document.getElementById('resultado')
 
   
-  if(tarifaTotal.value.length == ''){
+  if(potenciaTotal.value.length == '' || consumoDia.value.length == '' || consumoMes.value.length == ''){
     window.alert('Digite um número')
   }
-  else {
-    resultadoTotal.innerHTML = `Resultado: ${tarifaTotal.value}`
+  else if(nomeEletro.value == '') {
+    window.alert('Digite o nome do eletrodoméstico')
   }
-  
+  else {
+    /* resultadoTotal.innerHTML = `Resultado: ${potenciaTotal.value}` */
+
+    var calculoKW = potenciaTotal.value * consumoDia.value * consumoMes.value / 1000
+
+    resultadoTotal.innerText = `${nomeEletro.value} = ${calculoKW} kWh`
+  }
 }
 
+
+
+
+/* CONSUMO (kWh) = potência (W) x horas de uso por dia (h) x dias de uso no mês / 1000. */
+
 function limpar() {
+  var nomeEletro = document.getElementById('eletro')
+  var potenciaTotal = document.getElementById('potencia')
+  var consumoDia = document.getElementById('horasDia')
+  var consumoMes = document.getElementById('diasMes')
   var resultadoTotal = document.getElementById('resultado')
-  resultadoTotal.innerHTML = 'Resultado: '
+
+  nomeEletro.value = ''
+  potenciaTotal.value = ''
+  consumoDia.value = ''
+  consumoMes.value = ''
+  resultadoTotal.innerHTML = ''
 }
 
 //Botão Confirmar
-var botao = document.getElementById('botaoConfirmar')
-botao.addEventListener("click", confirmar)
+var botaoConf = document.getElementById('botaoConfirmar')
+botaoConf.addEventListener("click", confirmar)
 
 //Botão Limpar
-var botao2 = document.getElementById('botaoLimpar')
-botao2.addEventListener("click", limpar)
+var botaoLimp = document.getElementById('botaoLimpar')
+botaoLimp.addEventListener("click", limpar)
